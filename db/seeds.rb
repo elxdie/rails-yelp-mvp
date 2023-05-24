af2530a72591ad816c93d70a5bcab50d053f3c34
Elodie Ravaton <elodiemegane012@gmail.com>
2023-05-24 14:20:50 +0400

    seeds faker done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+category = %w[chinese italian japanese french belgian]
+
+# Write a seed to insert 7 restaurants in the database
+puts 'Creating restaurants...'
+7.times do
+  Restaurant.create(
+    name: Faker::Name.name,
+    # puts full_address as .address does not exist in faker
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: category.sample
+  )
+end
+puts 'Finished!'
